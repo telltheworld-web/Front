f44782cdeeba85f24e6a803e49107a640b6a03d3
yujeong <dbwjda0201@naver.com>
2025-05-19 01:10:27 +0900

    Add articl3,4

--- src/data/articles.js
+++ src/data/articles.js
@@ -3,7 +3,7 @@ const articles = [
   {
     id: 1,
     category: "노동",
-    title: "플랫폼 노동자도 데이터 라벨링을 하나?",
+    title: "quiz1-article",
     content:
       "AI를 훈련시키기 위해서는 정제된 데이터가 필요한데, 이 작업을 수행하는 노동자들이 플랫폼 노동자일 수 있습니다.",
     image:
@@ -13,7 +13,25 @@ const articles = [
   {
     id: 2,
     category: "아동",
-    title: "어린이들도 데이터 라벨링 노동을 하고 있을까?",
+    title: "quiz2-article",
+    content:
+      "일부 개발도상국에서는 아동이 저임금으로 데이터 라벨링 작업에 투입되기도 하며, 이는 윤리적 문제를 불러일으키고 있습니다.",
+    image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcvEICd%2Fbtstay1KpDX%2FNHlOrHG1cQyMzDwXiMKUFK%2Fimg.jpg",
+    link: "https://example.com/child-labor-article",
+  },
+  {
+    id: 3,
+    category: "아동",
+    title: "quiz3-article",
+    content:
+      "일부 개발도상국에서는 아동이 저임금으로 데이터 라벨링 작업에 투입되기도 하며, 이는 윤리적 문제를 불러일으키고 있습니다.",
+    image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcvEICd%2Fbtstay1KpDX%2FNHlOrHG1cQyMzDwXiMKUFK%2Fimg.jpg",
+    link: "https://example.com/child-labor-article",
+  },
+  {
+    id: 4,
+    category: "아동",
+    title: "quiz4-article",
     content:
       "일부 개발도상국에서는 아동이 저임금으로 데이터 라벨링 작업에 투입되기도 하며, 이는 윤리적 문제를 불러일으키고 있습니다.",
     image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcvEICd%2Fbtstay1KpDX%2FNHlOrHG1cQyMzDwXiMKUFK%2Fimg.jpg",
@@ -22,3 +40,4 @@ const articles = [
 ];
 
 export default articles;
+
